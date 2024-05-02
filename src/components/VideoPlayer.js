6d34b35d8b6815c92aa286fb6d5aaef400e56fae
Ethan Wolfgang Seaholm <71782610+ethanseaholm@users.noreply.github.com>
2024-05-02 13:51:17 -0500

    VideoLoader deletion & VideoPlayer updates

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,18 +1,16 @@
 import { useEffect, useState } from "react";
-import VideoLoader from "./VideoLoader";
 import ErrorMessage from "./ErrorMessage";
 import { Link } from "react-router-dom";
 
 /**
  * This component renders the homepage including a YouTube video player containing Sensational Chicago's latest video and its title.
- * While data is being fetched from the YouTube API, the VideoLoader placeholder is conditionally rendered.
+ * While data is being fetched from the YouTube API, a loader placeholder is conditionally rendered.
  * @returns {ReactNode} A React element that renders a YouTube video player containing the latest video and its title from Sensational Chicago's YouTube channel.
  */
 
 export default function VideoPlayer() {
   const [videoTitle, setVideoTitle] = useState("");
   const [videoId, setVideoId] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [videoUrl, setVideoUrl] = useState("");
 
@@ -25,70 +23,70 @@ export default function VideoPlayer() {
    * @function fetchVid
    */
 
-  useEffect(function () {
-    const controller = new AbortController();   // early AbortController implementation in case of future searchable videos usage
+  useEffect(
+    function () {
+      const controller = new AbortController(); // early AbortController implementation in case of future searchable video functionality
 
-    async function fetchVid() {
-      try {
-        setIsLoading(true);
+      async function fetchVid() {
+        try {
+          const res = await fetch(
+            `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=1&playlistId=${playlistId}&key=${apiKey}`,
+            { signal: controller.signal }
+          ); // fetches Sensational Chicago's latest video and its title
 
-        const res = await fetch(
-          `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=1&playlistId=${playlistId}&key=${apiKey}`,
-          { signal: controller.signal }
-        );    // fetches Sensational Chicago's latest video and its title
+          await new Promise((resolve) => setTimeout(resolve, 1500)); // sets a short timeout to ensure loader is rendered briefly
 
-        await new Promise((resolve) => setTimeout(resolve, 2000));    // sets a short timeout to ensure VideoLoader is rendered briefly
+          if (!res.ok)
+            // if an error occurs while fetching data from the YouTube API, the error message is rendered
+            throw new Error(
+              "Something went wrong with fetching our latest video"
+            );
 
-        if (!res.ok)    // if an error occurs while fetching data from the YouTube API, the error message is rendered
-          throw new Error(
-            "Something went wrong with fetching our latest video"
-          );
+          const data = await res.json();
+          if (data.items.length === 0) throw new Error("No videos found"); // if fetching data from the YouTube API returns nothing, the error message is rendered
 
-        const data = await res.json();
-        if (data.items.length === 0) throw new Error("No videos found");    // if fetching data from the YouTube API returns nothing, the error message is rendered
+          const latestVideo = data.items[0];
+          const latestVideoTitle = latestVideo.snippet.title;
+          const latestVideoId = latestVideo.snippet.resourceId.videoId;
+          const latestVideoUrl =
+            "https://www.youtube.com/watch?v=" + latestVideoId;
 
-        // setError("");
-
-        const latestVideo = data.items[0];
-        const latestVideoTitle = latestVideo.snippet.title;
-        const latestVideoId = latestVideo.snippet.resourceId.videoId;
-        const latestVideoUrl =
-          "https://www.youtube.com/watch?v=" + latestVideoId;
-
-        setVideoTitle(latestVideoTitle);
-        setVideoId(latestVideoId);
-        setVideoUrl(latestVideoUrl);
-
-      } catch (err) {
-        if (err.name !== "AbortError") {
-          console.log(err.message);
-          setError(err.message);
+          setVideoTitle(latestVideoTitle);
+          setVideoId(latestVideoId);
+          setVideoUrl(latestVideoUrl);
+        } catch (err) {
+          if (err.name !== "AbortError") {
+            console.log(err.message);
+            setError(err.message);
+          }
         }
-      } finally {
-        setIsLoading(false);    // if data is successfully fetched from the YouTube API and the timeout is resolved, render the VideoPlayer component
       }
-    }
-    fetchVid();
+      fetchVid();
 
-    return function () {
-      controller.abort();
-    };
-  }, [apiKey, playlistId]);   // if the apiKey or playlistId changes, a different video will be rendered (i.e., a new video is uploaded)
+      return function () {
+        controller.abort();
+      };
+    },
+    [apiKey, playlistId] // if the apiKey or playlistId changes, a different video will be rendered (i.e., a new video is uploaded)
+  );
 
   return (
     <>
-      {!isLoading && !error && (    // if data is successfully fetched from the YouTube API without any errors and the timeout is resolved, render the VideoPlayer component
+      {!error && ( // conditionally renders the VideoPlayer component if no errors occur during YouTube API data fetching
         <>
           <div className="video-player-container">
             <h2>
-              {videoTitle ? (   // renders the latest video's title
+              {!videoTitle ? ( // if data is currently being fetched from the YouTube API, renders the loader placeholder
+                <span className="loader-text">
+                  <i>Loading our latest video...</i>
+                </span>
+              ) : (
+                // once data has been fetched from the YouTube API, renders Sensational Chicago's latest video and its title
                 <div>
                   <Link to={videoUrl} target="_blank">
                     <span>{videoTitle}</span>
                   </Link>
                 </div>
-              ) : (
-                <VideoLoader />   // if data is currently being fetched from the YouTube API, render the VideoLoader component
               )}
             </h2>
             <iframe
