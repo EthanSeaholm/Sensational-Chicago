b3dd29f157616e3094427fa4753f65a7b30b9aad
Ethan Wolfgang Seaholm <71782610+ethanseaholm@users.noreply.github.com>
2024-05-13 17:57:23 -0500

    styling and gallery changes

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,14 +1,14 @@
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 
-import photo1 from "../gallery-resources/photos/p1.jpg";
+// import photo1 from "../gallery-resources/photos/p1.jpg";
 import photo2 from "../gallery-resources/photos/p2.jpg";
-import photo3 from "../gallery-resources/photos/p3.jpg";
+// import photo3 from "../gallery-resources/photos/p3.jpg";
 import photo4 from "../gallery-resources/photos/p4.jpg";
-import photo5 from "../gallery-resources/photos/p5.jpg";
+import photo5 from "../gallery-resources/photos/p5.jpeg";
 import photo6 from "../gallery-resources/photos/p6.jpg";
-import photo7 from "../gallery-resources/photos/p7.jpg";
-import photo8 from "../gallery-resources/photos/p8.JPG";
+// import photo7 from "../gallery-resources/photos/p7.jpg";
+// import photo8 from "../gallery-resources/photos/p8.JPG";
 import photo9 from "../gallery-resources/photos/p9.jpg";
 import photo10 from "../gallery-resources/photos/p10.jpg";
 import photo11 from "../gallery-resources/photos/p11.JPG";
@@ -16,6 +16,10 @@ import photo12 from "../gallery-resources/photos/p12.jpg";
 import photo13 from "../gallery-resources/photos/p13.JPG";
 import photo14 from "../gallery-resources/photos/p14.jpg";
 import photo15 from "../gallery-resources/photos/p15.jpg";
+import photo16 from "../gallery-resources/photos/p16.jpg";
+import photo17 from "../gallery-resources/photos/p17.jpg";
+import photo18 from "../gallery-resources/photos/p18.jpg";
+import photo19 from "../gallery-resources/photos/p19.jpg";
 import { useState } from "react";
 
 /**
@@ -26,11 +30,22 @@ import { useState } from "react";
  */
 
 const photos = [
+  // {
+  //   id: 1,
+  //   photoLink: photo16,
+  //   description:
+  //     "Josh & Rex - Huffman Skatepark in West Dundee, IL, Summer 2022.",
+  // },
+  // {
+  //   id: 17,
+  //   photoLink: photo17,
+  //   description:
+  //     "(L to R) Thomas, Josh, Rex, & Jonathan - Camping in Wisconsin, Summer 2022.",
+  // },
   {
-    id: 1,
-    photoLink: photo1,
-    description:
-      "Josh & Rex - Huffman Skatepark in West Dundee, IL, Summer 2022.",
+    id: 19,
+    photoLink: photo19,
+    description: "Thomas & Rex - Camping in Wisconsin, Summer 2022.",
   },
   {
     id: 2,
@@ -39,9 +54,10 @@ const photos = [
       "(L to R) Neil, Jonathan, Thomas, Ethan, & Josh - Josh's 22nd birthday at his house, Summer 2021.",
   },
   {
-    id: 3,
-    photoLink: photo3,
-    description: "Josh & Ethan - Josh's park, Winter 2017.",
+    id: 16,
+    photoLink: photo16,
+    description:
+      "(L to R) Jonathan, Devan, Josh, Thomas, & Rex - Fry the Coop, Summer 2021.",
   },
   {
     id: 4,
@@ -53,24 +69,35 @@ const photos = [
     id: 5,
     photoLink: photo5,
     description:
-      "(L to R) Rex, Ethan, Josh, Thomas, & Jonathan - Volleyball Tuesday at Sideouts, Summer 2022.",
+      "(L to R) Josh, Ethan, Thomas, & Rex - Volleyball Tuesday at Sideouts, Summer 2023.",
   },
   {
     id: 6,
     photoLink: photo6,
     description: "Neil & Jonathan - Josh's park, Summer 2019.",
   },
+  // {
+  //   id: 7,
+  //   photoLink: photo7,
+  //   description:
+  //     "(L to R) Rex, Josh, Thomas, & Jonathan - New Year's Eve in Marco Island, FL, 2020.",
+  // },
   {
-    id: 7,
-    photoLink: photo7,
-    description:
-      "(L to R) Rex, Josh, Thomas, & Jonathan - New Year's Eve in Marco Island, FL, 2020.",
+    id: 18,
+    photoLink: photo18,
+    description: "Jonathan & Josh - Fishing in Elgin, IL, Summer 2022.",
   },
+  // {
+  //   id: 8,
+  //   photoLink: photo8,
+  //   description:
+  //     "(L to R) Jonathan, Ethan, & Josh - Jonathan's place in Black River Falls, WI, Summer 2018.",
+  // },
   {
-    id: 8,
-    photoLink: photo8,
+    id: 17,
+    photoLink: photo17,
     description:
-      "(L to R) Jonathan, Ethan, & Josh - Jonathan's place in Black River Falls, WI, Summer 2018.",
+      "(L to R) Jonathan, Josh, Rex, Thomas, & Neil - Nowadays Premiere at Neil's house, Winter 2021.",
   },
   {
     id: 9,
